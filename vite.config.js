39c4baea1a241aa09ea016299c441a47f5076f86
Rob OConnell <roconnell@smith.edu>
2026-06-22 10:26:53 -0400

    Align local dev base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/gadgets-to-go/',
-  plugins: [react(), mkcert()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_BASE_URL || '/gadgets-to-go/',
+    plugins: [react(), mkcert()],
+  }
 })
